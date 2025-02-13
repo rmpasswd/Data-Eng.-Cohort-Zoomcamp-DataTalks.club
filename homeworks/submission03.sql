4adb804b4bb0392ad298e187f6d24ce166bef61e
Mahin Ahmad <35218856+rmpasswd@users.noreply.github.com>
2025-02-13 11:39:18 +0600

    Create submission03.sql

---
+++ homeworks/submission03.sql
@@ -0,0 +1,4 @@
+-- QuestionQuestion 1. Count of records for the 2024 Yellow Taxi Data (1 point)
+SELECT COUNT(*) FROM `data-eng-450213.ny_taxi_2024.ny_taxi_2024_jan_jun` 
+
+-- Question 2. Estimated amount of data (1 point)
